f4c26abd444462d685bfa66f27543ef992bd1ef1
cookie <392183@qq.com>
2018-03-19 17:05:21 +0800

    no message

--- src/core/api/general.py
+++ src/core/api/general.py
@@ -2,7 +2,7 @@ import json
 import logging
 from django.http import HttpResponse
 from rest_framework.response import Response
-from libs import gen_ddl, baseview, con_database
+from libs import baseview, con_database
 from core.task import grained_permissions
 from core.models import (
     DatabaseList,
@@ -23,6 +23,7 @@ class addressing(baseview.BaseView):
     '''
 
     :argument 连接名 库名 表名 字段名 索引名 api接口
+
 
     '''
 
@@ -152,96 +153,3 @@ class addressing(baseview.BaseView):
                     return Response(e)
 
 
-class gensql(baseview.BaseView):
-    '''
-    put 生成DDL语句 生成索引语句
-
-    '''
-
-    def put(self, request, args=None):
-
-        if args == "sql":
-            try:
-                data = request.data['data']
-                data = json.loads(data)
-                base = request.data['basename']
-            except KeyError as e:
-                CUSTOM_ERROR.error(f'{e.__class__.__name__}: {e}')
-            else:
-                info1 = []
-                try:
-                    for i in data:
-                        if 'edit' in i.keys():
-                            info = gen_ddl.create_sql(select_name='edit',
-                                                      column_name=i['edit']['Field'],
-                                                      column_type=i['edit']['Type'],
-                                                      default=i['edit']['Default'],
-                                                      comment=i['edit']['Extra'],
-                                                      null=i['edit']['Null'],
-                                                      table_name=i['table_name'],
-                                                      base_name=base)
-                            info1.append(info)
-
-                        elif 'del' in i.keys():
-                            info = gen_ddl.create_sql(select_name='del',
-                                                      column_name=i['del']['Field'],
-                                                      table_name=i['table_name'],
-                                                      base_name=base)
-                            info1.append(info)
-                        elif 'add' in i.keys() and i['add'] != []:
-                            for n in i['add']:
-                                info = gen_ddl.create_sql(select_name='add',
-                                                          column_name=n['Field'],
-                                                          base_name=base,
-                                                          column_type=n['Type'],
-                                                          default=n['Default'],
-                                                          comment=n['Extra'],
-                                                          null=n['Null'],
-                                                          table_name=i['table_name'])
-
-                                info1.append(info)
-                    return Response(info1)
-                except Exception as e:
-                    CUSTOM_ERROR.error(f'{e.__class__.__name__}: {e}')
-                    return HttpResponse(status=500)
-
-        elif args == "index":
-            try:
-                data = json.loads(request.data['data'])
-            except KeyError as e:
-                CUSTOM_ERROR.error(f'{e.__class__.__name__}: {e}')
-            else:
-                info1 = []
-                try:
-                    for i in data:
-                        if 'delindex' in i.keys():
-                            info = gen_ddl.index(select_name='delindex',
-                                                 key_name=i['delindex']['key_name'],
-                                                 table_name=i['table_name'])
-                            info1.append(info)
-                        elif 'addindex' in i.keys() and i['addindex'] != []:
-                            for n in i['addindex']:
-                                if n['fulltext'] == "YES":
-                                    info = gen_ddl.index(table_name=i['table_name'],
-                                                         column_name=n['column_name'],
-                                                         key_name=n['key_name'],
-                                                         fulltext=n['fulltext'],
-                                                         select_name='addindex')
-                                    info1.append(info)
-                                elif n['Non_unique'] == "YES":
-                                    info = gen_ddl.index(select_name='addindex',
-                                                         key_name=n['key_name'],
-                                                         non_unique='unique',
-                                                         column_name=n['column_name'],
-                                                         table_name=i['table_name'])
-                                    info1.append(info)
-                                else:
-                                    info = gen_ddl.index(select_name='addindex',
-                                                         key_name=n['key_name'],
-                                                         column_name=n['column_name'],
-                                                         table_name=i['table_name'])
-                                    info1.append(info)
-                    return Response(info1)
-                except Exception as e:
-                    CUSTOM_ERROR.error(f'{e.__class__.__name__}: {e}')
-                    return HttpResponse(status=500)
