e2095a300530f08536a613d23cdf6ff0f9680ea9
Thomas Lee <litao.git@gmail.com>
2018-05-23 11:22:46 +0800

    auto dectect http schema

--- webpage/src/libs/util.js
+++ webpage/src/libs/util.js
@@ -7,9 +7,9 @@ util.title = function (title) {
   window.document.title = title;
 };
 
-util.url = 'http://' + document.domain + ':8000/api/v1'
+util.url = location.protocol + "//" + document.domain + ':8000/api/v1'
 
-util.auth = 'http://' + document.domain + ':8000/api-token-auth/'
+util.auth = location.protocol + "//" + document.domain + ':8000/api-token-auth/'
 
 util.computer_room = ['AWS', 'Aliyun', 'Own', 'Other']
 
