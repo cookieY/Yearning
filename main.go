fb70ebec8dba32d842836fe13d4c6d50309bb1c5
HenryYee <henry@yearning.io>
2020-10-27 10:13:18 +0800

    Yearning 2.3.0 GA

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ var (
 )
 
 func usage() {
-	_, err := fmt.Fprintf(os.Stderr, `version: Yearning/2.3.0 author: HenryYee
+	_, err := fmt.Fprintf(os.Stderr, `version: Yearning/2.3.0 GA author: HenryYee
 Usage: Yearning [-m migrate] [-p port] [-s start] [-b web-bind] [-h help] [-c config file]
 
 Options:
